1e0cba303439fd104ec0f1b745acb815efb28eab
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2020-08-06 12:40:15 +0300

    savecommentsToDB

--- app.rb
+++ app.rb
@@ -21,6 +21,14 @@ configure do
      created_date DATE,
      content TEXT
    )'
+   @db.execute 'CREATE TABLE IF NOT EXISTS
+  Comments 
+  (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     created_date DATE,
+     content TEXT,
+     post_id INTEGER
+   )'
 end
 
 get '/' do
@@ -58,6 +66,9 @@ post '/details/:post_id' do
   post_id = params[:post_id]
   content = params[:content]
 
-  erb "You typed content #{content} for post #{post_id}"
+  @db.execute 'insert into Comments (content, created_date, post_id ) values (?,datetime(), ?)',[content, post_id]
 
+  erb "You typed content #{content} for post #{post_id}"
+  
+  redirect to("/details/" + post_id)
 end
