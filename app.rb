cf796e18e520594250ccd703c8ae9aedf272e172
LeonidWebDev <55695468+LeonidWebDev@users.noreply.github.com>
2020-08-06 10:20:34 +0300

    initDB

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new "leprosorium.db"
+  db.results_as_hash = true
+end
+
+before do
+  init_db
+end
 
 get '/' do
   erb '<h3>Hello from sinatra</h3>'
@@ -8,5 +18,10 @@ end
 
 get '/new' do
   erb :new
+end
+
+post '/new' do
+  content = params[:content]
+  erb "Thanks for your'e post"
 end
 
